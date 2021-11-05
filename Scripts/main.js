60a008b9a2a73ed94140227859c55907ce50ebaf
SamFerreira <sammyferr2403@gmail.com>
2021-11-04 21:20:58 -0400

    Stretched a cube

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,29 +1,33 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+cube.position.set(3, 0, 0);
 scene.add( cube );
 
 camera.position.z = 5;
 
-var animate = function () {
+const animate = function () {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+	
+	if(cube.scale.z <= 20){
+		cube.scale.setZ(cube.scale.z + 0.5);
+	}
+	
 
 	renderer.render( scene, camera );
 };
 
 animate();
-
 
 
 window.onresize = function () {
