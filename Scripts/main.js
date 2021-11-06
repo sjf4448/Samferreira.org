1d4aaa4eeb252e4567222455631eeda4758cba42
SamFerreira <sammyferr2403@gmail.com>
2021-11-06 08:28:55 -0400

    comments

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,29 +5,41 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//creates cube object
+//3D shapes require a geometry and material these go into a Mesh
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 cube.position.set(3, 0, 0);
-scene.add( cube );
+
+//Adds cube to scene
+scene.add(cube);
 
 camera.position.z = 5;
 
 const animate = function () {
 	requestAnimationFrame( animate );
 
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+	//rotates cube object
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 	
-	if(cube.scale.z <= 20){
-		cube.scale.setZ(cube.scale.z + 0.5);
-	}
+	//Makes the cube stretch on Z-Axis
+	// if(group.scale.z <= 20){
+	// 	group.scale.setZ(group.scale.z + 0.5);
+	// }
 	
 
 	renderer.render( scene, camera );
 };
 
 animate();
+
+//Need to create a function for generating cubes
+//Need to put my name somewhere
+//Add a button to activate the effect
+//Planets for portfolio pages?
+//Goal: Star wars space jump effect
 
 
 window.onresize = function () {
