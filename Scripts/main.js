0b5a0e959e2b73312a411c7885f85c0fdfce05d4
SamFerreira <sammyferr2403@gmail.com>
2021-11-06 23:22:27 -0400

    Space

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,28 +1,80 @@
+//creates scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
+//Creates renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//creates cube object
-//3D shapes require a geometry and material these go into a Mesh
+//idk why but when I delete this everything breaks
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-cube.position.set(3, 0, 0);
-
-//Adds cube to scene
-scene.add(cube);
 
 camera.position.z = 5;
 
+const space = new THREE.Group();
+
+//Used to make the cubes that will be used for stars
+function makeStar(posX, posY, posZ){
+
+	var geometry = new THREE.SphereGeometry()
+	var material = new THREE.MeshBasicMaterial(({ color: 0xffffff }));
+
+	//for(var i = 0; i <= n; i++){
+		var mesh = new THREE.Mesh(geometry, material);
+		mesh.position.x = posX;
+		mesh.position.y = posY;
+		mesh.position.z = posZ;
+
+
+		space.add(mesh);
+	//}
+}
+
+function getRandomArbitrary(min, max) {
+	var num = Math.random() * (max - min) + min
+	if(num > 0 && num < 10){
+		num += 50
+	}
+	else if(num < 0 && num > -10){
+		num -= 50
+	}
+	return num;
+  }
+
+function buildSpace(n){
+	for(var i = 0; i <= n; i++){
+		makeStar(getRandomArbitrary(-1000, 1000), getRandomArbitrary(-1000, 1000), getRandomArbitrary(-1000, 10));
+	}
+}
+
+//number of stars
+const stars = 5000;
+
+buildSpace(stars);
+scene.add(space);
+//switch to true for animation
+var jump = true;
+
+//Need to put my name somewhere
+//Add a button to activate the effect
+//Planets for portfolio pages?
+//Goal: Star wars space jump effect
+
+//infinte animation loop
 const animate = function () {
 	requestAnimationFrame( animate );
 
-	//rotates cube object
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	if(jump == true){
+		//animates every object in space
+		for (var i = 0; i <= stars; i++){
+			space.children[i].scale.z += 0.1;
+			space.children[i].position.z += 0.7;
+		}
+	}
 	
 	//Makes the cube stretch on Z-Axis
 	// if(group.scale.z <= 20){
@@ -35,13 +87,9 @@ const animate = function () {
 
 animate();
 
-//Need to create a function for generating cubes
-//Need to put my name somewhere
-//Add a button to activate the effect
-//Planets for portfolio pages?
-//Goal: Star wars space jump effect
 
 
+//When window is resized stuff dont break
 window.onresize = function () {
 
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -51,4 +99,5 @@ window.onresize = function () {
 
 };
 
+//hides srollbar
 document.body.style.overflow = 'hidden';
