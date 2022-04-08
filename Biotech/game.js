84171f32abc04583ad0340b6f951ac8e7bc08b9d
Samuel Ferreira <57544768+Sammy2403@users.noreply.github.com>
2022-04-08 08:56:00 -0400

    silly mistake

--- Biotech/game.js
+++ Biotech/game.js
@@ -71,7 +71,7 @@ function drawGreenBlob(){
 function drawScore(){
     ctx.fillStyle = "green";
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2)
+    ctx.arc(0, 0, radius, 0, Math.PI * 2)
     ctx.fill();
 }
 
